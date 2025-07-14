d95a53c41fc282b49654cafc3476fec743d57244
Rain.Hu <intervalrain@gmail.com>
2025-07-14 11:14:36 +0800

    added queue-group and channel attribute

--- src/EdgeSync.ServiceFramework.Abstractions/Attributes/QueueGroupAttribute.cs
+++ src/EdgeSync.ServiceFramework.Abstractions/Attributes/QueueGroupAttribute.cs
@@ -1,7 +1,7 @@
 namespace EdgeSync.ServiceFramework.Abstractions.Attributes;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-public class ChannelAttribute(string name) : Attribute
+public class QueueGroupAttribute(string name) : Attribute
 {
     public string Name { get; } = name;
 } 
